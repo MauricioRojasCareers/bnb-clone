adb9d568b4ad992c0dcf09de01afcdb99f5e2760
starling <starling@starlings-MBP.lan>
2023-04-17 18:25:55 -0500

    fixed bug

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import { CategoryInput } from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
+import Counter from "../inputs/Counter";
 
 enum STEPS {
   CATEGORY = 0,
@@ -34,7 +35,7 @@ export const RentModal = () => {
     defaultValues: {
       category: "",
       location: null,
-      guessCount: 1,
+      guestCount: 1,
       roomCount: 1,
       bathroomCount: 1,
       imageSrc: "",
@@ -46,6 +47,7 @@ export const RentModal = () => {
 
   const category = watch("category");
   const location = watch("location");
+  const guestCount = watch("guestCount");
 
   const Map = useMemo(
     () =>
@@ -127,6 +129,22 @@ export const RentModal = () => {
           onChange={(value) => setCustomValue("location", value)}
         />
         <Map center={location?.latlng} />
+      </div>
+    );
+  }
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Share some basics about your place"
+          subtitle="What amenities do you have?"
+        />
+        <Counter
+          title="Number of guests"
+          subtitle="How many guests do you allow?"
+          value={guestCount}
+          onChange={(value) => setCustomValue("guestCount", value)}
+        />
       </div>
     );
   }
